98602bc09fa6229e4703029f70631ec67d7289d0
ni2021tesh <niteshjha2021@gmail.com>
2019-03-21 23:57:40 +0530

    remember me added

--- src/main/java/info/niteshjha/securityConfig/UserSecurityConfig.java
+++ src/main/java/info/niteshjha/securityConfig/UserSecurityConfig.java
@@ -1,12 +1,17 @@
 package info.niteshjha.securityConfig;
 
 import info.niteshjha.config.UserService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 
 @EnableWebSecurity
 public class UserSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,13 +22,25 @@ public class UserSecurityConfig extends WebSecurityConfigurerAdapter {
         this.userService = userService;
     }
 
-    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence userEnteredPassword) {
+                return getMd5(userEnteredPassword.toString());
+            }
+
+            @Override
+            public boolean matches(CharSequence userEnteredPassword, String storedPassword) {
+                return Objects.equals(getMd5(userEnteredPassword.toString()), getMd5(storedPassword));
+            }
+        };
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {  //@formatter:off
 
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
 
     @Override
@@ -48,9 +65,43 @@ public class UserSecurityConfig extends WebSecurityConfigurerAdapter {
                             .logoutUrl("/doLogout")
                             .logoutSuccessUrl("/login?logout")
 
-                .and().csrf().disable();
+                .and().csrf().disable()
+                .rememberMe().key("myappkey");
 
         //@formatter:on
     }
+
+
+    private static String getMd5(String input) {
+        try {
+            // Static getInstance method is called with hashing SHA
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // digest() method called
+            // to calculate message digest of an input
+            // and return array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            System.out.println("Exception thrown"
+                    + " for incorrect algorithm: " + e);
+            return null;
+        }
+    }
+
 
 }
